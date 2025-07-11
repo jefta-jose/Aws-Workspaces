2be0dab58556dc8ac48381432332c72de103fa18
jefta-jose <ndegwajeff4@gmail.com>
2025-07-11 18:46:59 +0300

    using unique names

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -2,19 +2,19 @@
 
 module "iam_user"{
     source = "../../modules/iam_user"
-    user_name = "dev-user"
+    user_name = "jeff-ndegwa-254-dev-user"
 }
 
 module "lambda_function"{
     source = "../../modules/lambda"
-    lambda_function_name = "dev-lambda-function"
-    lambda_invoke_role_name = "dev-lambda-invoke-role"
+    lambda_function_name = "jeff-ndegwa-254-dev-lambda-function"
+    lambda_invoke_role_name = "jeff-ndegwa-254-dev-lambda-invoke-role"
 }
 
 module "s3_bucket"{
     source = "../../modules/s3"
-    bucket_name = "dev-s3-bucket"
-    s3_bucket_access_policy_name = "dev-s3-policy"
+    bucket_name = "jeff-ndegwa-254-dev-s3-bucket"
+    s3_bucket_access_policy_name = "jeff-ndegwa-254-dev-s3-policy"
     user_name = module.iam_user.s3_upload_user_name
     lambda_function_arn = module.lambda_function.lambda_function_arn
     lambda_function_name = module.lambda_function.lambda_function_name
